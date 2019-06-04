6bb91ee29c132952e163734c27f873ab2ccdb268
Anna Bertolani <bertolani.anna@gmail.com>
2019-06-04 13:30:20 +0200

    Movimento personaggio e camera + Sparo

---
+++ Assets/Scripts/Chain.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chain : MonoBehaviour
+{
+    [SerializeField] float MovementSpeed, MaxDistance;
+    Vector3 TargetPoint;
+
+    public void Shoot()
+    {
+        Move();
+    }
+
+    public void SetTargetPoint(Vector3 _targetPosition)
+    {
+        TargetPoint = _targetPosition;
+    }
+
+    public IEnumerator Move()
+    {
+        Vector3 startPosition = transform.position;
+        float distanceFromShootPosition = 0;
+        while (distanceFromShootPosition < MaxDistance)
+        {
+            transform.Translate(Vector3.forward, Space.Self);
+            distanceFromShootPosition = Vector3.Distance(startPosition, transform.position);
+            yield return null;
+        }
+        Destroy(gameObject);
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] Camera camera;
+    [SerializeField] Image AimImage;
+    [SerializeField] Chain chainPrefab;
+    [SerializeField] Transform ShootPoint;
+
+    [SerializeField] float MovementSpeed;
+    [SerializeField] float RotationSpeed;
+    [SerializeField] float ReloadTime;
+
+    bool canShoot;
+
+    float h, v;
+    Vector3 inputDirection;
+    float mouseX, mouseY;
+
+    private void Awake()
+    {
+        AimImage.gameObject.SetActive(false);
+        canShoot = true;
+    }
+
+    private void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        inputDirection = new Vector3(h, 0, v);
+
+        camera.transform.Rotate(new Vector3(mouseY, 0, 0) * RotationSpeed, Space.Self);
+        Movement();
+        if (Input.GetMouseButton(1))
+        {
+            Aim();
+        }
+        else
+        {
+            if (AimImage.gameObject.activeInHierarchy)
+            {
+                AimImage.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void Movement()
+    {
+        transform.Translate(MovementSpeed * inputDirection * Time.deltaTime, Space.Self);
+        transform.Rotate(new Vector3(0, mouseX, 0) * RotationSpeed);
+    }
+
+    void Aim()
+    {
+        AimImage.gameObject.SetActive(true);
+        if (Input.GetMouseButton(0) && canShoot)
+        {
+            canShoot = false;
+            StartCoroutine(Reload());
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Chain shootedChain = Instantiate(chainPrefab, ShootPoint.position, transform.rotation, transform);
+        StartCoroutine(shootedChain.Move());
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(ReloadTime);
+        canShoot = true;
+    }
+}
